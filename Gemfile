bb46016154fb5e2c1b836b71b1dd6aa0ef437355
Nicholas Suski <nick@suskitech.org>
2018-07-01 01:47:21 -0600

    Add gemfile, babel, autoprefixing

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org' do
+  gem "jekyll", '3.8.0'
+  gem "sass", '3.5.6'
+  gem "mini_magick", '4.8.0'
+end
+
+group :jekyll_plugins do
+  gem "octopress-autoprefixer"
+  gem "jekyll-babel"
+end
--- src/js/actions.js
+++ src/js/actions.js
@@ -5,12 +5,14 @@ jQuery(document).ready(function($){
 		var j;
 		var hexes = this.match(/.{1,4}/g) || [];
 		var back = "";
+
 		for(j = 0; j<hexes.length; j++) {
 			back += String.fromCharCode(parseInt(hexes[j], 16));
 		}
 
 		return back;
 	}
+	
 	$('body').find('.site-email-address').attr('href', 'mailto:'+$('.site-email-address').data('email').hexDecode()+'@'+$('.site-email-address').data('domain').hexDecode());
 
 });
