6172dd8b12741d149f02f6ae8c044d2c405a418d
Nicholas Suski <nick@suskitech.org>
2021-07-20 19:27:18 -0600

    use API for iching interface

--- src/iching/index.php
+++ src/iching/index.php
@@ -25,31 +25,100 @@
 			}else{
 				echo '<div class="container margin-bottom-15">';
 			}
-		?>
-			<?php
-				$isSubmitted = false;
-				if(isset($_GET["string"])){
-					$theString = $_GET["string"];
-				}
-				if(isset($theString)){
-					$isSubmitted = true;
-				}
 
-				if($isSubmitted == true){
-					//GET string var from url
-					$getURLarg = " " . $_GET["string"];
-					//escape statement for shell
-					$command = escapeshellcmd('python PhPyChing.py' . (string)$getURLarg);
-					//execute command plus string var as argument
-					$output = shell_exec($command);
-					//Print the results from python
-					echo $output;
-					echo '<br><a href="/iching/"> < Back</a>';
-				}else{
-					echo '<h3 class="text-center">Enter Your Query:</h3><div class="row"><div class="col-sm-6 col-sm-offset-3"><form action="" class="text-center"><input class="form-control" name="string"></div><div class="col-sm-4 col-sm-offset-4"><button class="btn btn-primary btn-block" type="submit">Cast</button></form></div>';	
+			$isSubmitted = false;
+			if(isset($_GET["string"])){
+				$theString = $_GET["string"];
+			}
+			if(isset($theString)){
+				$isSubmitted = true;
+			}
+
+			if($isSubmitted == true):
+				// create & initialize a curl session
+				$curl = curl_init();
+
+				// set our url with curl_setopt()
+				curl_setopt($curl, CURLOPT_URL, "http://suskitech.org/ichingapi/?cast=yarrow");
+
+				// return the transfer as a string, also with setopt()
+				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+				// curl_exec() executes the started curl session
+				// $output contains the output string
+				$result = json_decode(curl_exec($curl));
+
+				// close curl resource to free up system resources
+				// (deletes the variable made by curl_init)
+				curl_close($curl);
+		?>
+
+			<h1>
+				<?php echo $result->title; ?>
+			</h1>
+
+			<div class="hexagram">
+				<?php
+					echo $result->numbers;
+
+					for ($i = strlen($result->numbers) - 1; $i >= 0; $i--){
+						switch ($result->numbers[$i]) {
+							case '6':
+								echo '<div class="oldyang">&nbsp;</div>';
+								break;
+							case '7':
+								echo '<div class="yang">&nbsp;</div>';
+								break;
+							case '8':
+								echo '<div class="yin">&nbsp;</div>';
+								break;
+							case '9':
+								echo '<div class="oldyin">&nbsp;</div>';
+								break;
+							default:
+								break;
+						}
+					}
+				?>
+			</div>
+
+			<div class="trigrams">
+				<div class="uppertrigram <?php echo $result->upperTrigram ?>">
+				<div class="lowertrigram <?php echo $result->lowerTrigram ?>">
+			</div>
+
+			<h3>The Judgement</h3>
+			<?php echo $result->judgement ?>
+
+			<h3>The Image</h3>
+			<?php echo $result->image ?>
+
+			<?php
+				if (!empty($result->lines)) {
+					echo '<h3>The Lines</h3>';
+					echo $result->lines;
 				}
 			?>
-			<div class="full-width pull-left">				
+
+			<br><a href="/iching/"> < Back</a>
+
+		<?php else: ?>
+
+				<h3 class="text-center">Enter Your Query:</h3>
+
+				<div class="row">
+					<div class="col-sm-6 col-sm-offset-3">
+						<form action="" class="text-center">
+							<input class="form-control" name="string">
+					</div>
+					<div class="col-sm-4 col-sm-offset-4">
+						<button class="btn btn-primary btn-block" type="submit">Cast</button>
+					</form>
+				</div>
+
+		<?php endif; ?>
+
+			<div class="full-width pull-left">
 				<div class="colorcube Earth">&nbsp;</div>
 				<div class="colorcube Mountain">&nbsp;</div>
 				<div class="colorcube Water">&nbsp;</div>
